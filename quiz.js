72dd4c4fc3abf7915a82a91e688c64f157cb8277
fiachmchh <fiachmchh@gmail.com>
2020-12-02 20:36:51 +0000

    second commit, ffirst stage of quiz complete

--- quiz.js
+++ quiz.js
@@ -12,6 +12,10 @@ let shuffledQuestions, currentQuestionIndex
 
 //OR...startButton.addEventListener('click', startGame (plus start timer function or new event listener))
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     //console.log('Started')
@@ -53,8 +57,33 @@ function resetState() {
     }
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
 
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
@@ -62,6 +91,44 @@ const questions = [
         question: 'what color are frogs?',
         answers: [
             { text: 'green', correct: true},
+            { text: 'red', correct: false},
+            { text: 'orange', correct: false},
+            { text: 'purple', correct: false}
+        ]
+    },
+    {
+        question: 'Who was the first man on the moon?',
+        answers: [
+            { text: 'Edwin "Buzz" Aldrin', correct: false},
+            { text: 'Neil Armstrong', correct: true},
+            { text: 'Alan B. Shepard Jr.', correct: false},
+            { text: 'George Bush Sr', correct: false}
+        ]
+    },
+    {
+        question: 'When was the electric guitar invented?',
+        answers: [
+            { text: '1929', correct: false},
+            { text: '1899', correct: false},
+            { text: '1931', correct: true},
+            { text: '1912', correct: false}
+        ]
+    },
+    {
+        question: 'What is the worlds largest country by landmass?',
+        answers: [
+            { text: 'Canada', correct: false},
+            { text: 'USA', correct: false},
+            { text: 'China', correct: false},
+            { text: 'Russia', correct: true}
+        ]
+    },
+    {
+        question: 'what color are frogs?',
+        answers: [
+            { text: 'green', correct: true},
+            { text: 'red', correct: false},
+            { text: 'red', correct: false},
             { text: 'red', correct: false}
         ]
     }
